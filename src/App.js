12a5ed03d4daf523589952b3b13b52a14b404ba2
Juzantino Junadi <JUZANTINOJ@GMAIL.COM>
2022-01-24 15:12:15 +0800

    test update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Tino Portfolio</h1>
       <h2>Welcome to my Page</h2>
+      <h3>NFT</h3>
+      <p>Welcome to my NFT space. Here I will post about what I will be creating and minting and make my NFT available to everyone. Enjoy!</p>
       Learn React (Deploy Test)
       <div>BaseURL: {config.baseURL}</div>
     </div>
